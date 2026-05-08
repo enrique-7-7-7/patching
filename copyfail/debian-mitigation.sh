b164808e88d58cc6f291ec54bfaf7f3b995099ae
enrique empuerto <enrique.empuerto@gmail.com>
2026-05-09 07:49:48 +0800

    Update debian-mitigation.sh to add module blacklist

--- copyfail/debian-mitigation.sh
+++ copyfail/debian-mitigation.sh
@@ -4,3 +4,4 @@ echo "install esp4 /bin/false" | sudo tee /etc/modprobe.d/dirty-frag.conf
 echo "install esp6 /bin/false" | sudo tee -a /etc/modprobe.d/dirty-frag.conf
 echo "install rxrpc /bin/false" | sudo tee -a /etc/modprobe.d/dirty-frag.conf
 sudo update-initramfs -u -k all
+rmmod esp4 esp6 rxrpc
