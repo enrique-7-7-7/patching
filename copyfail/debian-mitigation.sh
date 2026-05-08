80992dfdb5c9cc552d4dc8d8d75b03d504a826df
enrique empuerto <enrique.empuerto@gmail.com>
2026-05-09 07:48:03 +0800

    Update debian-mitigation.sh

--- copyfail/debian-mitigation.sh
+++ copyfail/debian-mitigation.sh
@@ -1,3 +1,5 @@
+#curl -fsSL https://raw.githubusercontent.com/enrique-7-7-7/patching/refs/heads/main/copyfail/debian-mitigation.sh | bash
+
 echo "install esp4 /bin/false" | sudo tee /etc/modprobe.d/dirty-frag.conf
 echo "install esp6 /bin/false" | sudo tee -a /etc/modprobe.d/dirty-frag.conf
 echo "install rxrpc /bin/false" | sudo tee -a /etc/modprobe.d/dirty-frag.conf
