a59734c80f27248787938dd5e1a33b82daa76804
enrique empuerto <enrique.empuerto@gmail.com>
2026-05-09 07:48:35 +0800

    Update RHEL-mitigation.sh

--- copyfail/RHEL-mitigation.sh
+++ copyfail/RHEL-mitigation.sh
@@ -1,1 +1,2 @@
+#curl -fsSL https://raw.githubusercontent.com/enrique-7-7-7/patching/refs/heads/main/copyfail/RHEL-mitigation.sh | bash
 echo -e "install esp4 /bin/false\ninstall esp6 /bin/false\ninstall rxrpc /bin/false" > /etc/modprobe.d/dirtyfrag.conf
